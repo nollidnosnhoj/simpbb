5763f2fb83d6e4690a5dd56355e4753dcdca8878
Randall D. Johnson <nollidnosnhoj@gmail.com>
2024-02-19 15:04:06 -1000

    add alpine collapse plugin

--- internal/partials/page_templ.go
+++ internal/partials/page_templ.go
@@ -10,7 +10,7 @@ import "context"
 import "io"
 import "bytes"
 
-func PageLayout() templ.Component {
+func PageLayout(title string) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -23,7 +23,20 @@ func PageLayout() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html><head><title>My Page</title><script src=\"/dist/js/vendor/htmx.min.js\"></script><script defer src=\"/dist/js/vendor/alpine.min.js\"></script></head><body>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html><head><title>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		var templ_7745c5c3_Var2 string
+		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(title)
+		if templ_7745c5c3_Err != nil {
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/partials/page.templ`, Line: 5, Col: 17}
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title><script src=\"/dist/js/vendor/htmx.min.js\"></script><script defer src=\"/dist/js/vendor/alpine-collapse.min.js\"></script><script defer src=\"/dist/js/vendor/alpine.min.js\"></script></head><body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
